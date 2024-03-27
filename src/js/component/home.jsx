cb2e1d5b7c4e31f8a3e6cad7f13d3206faff5b32
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-03-27 02:57:00 +0000

    Simple Counter using React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
+			<h1 className="text-center mt-5">Counter using React by Carlos Escalante!</h1>
 			<p>
 				<img src={rigoImage} />
 			</p>
